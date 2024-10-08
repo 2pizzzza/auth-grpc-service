fcae2f416d538812daa452a401239769f202795e
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-08 20:43:28 +0600

    firest setup

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -4,11 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/2pizzzza/authGrpc/internal/config"
-	"github.com/2pizzzza/authGrpc/internal/lib/logger/sl"
 	_ "github.com/lib/pq"
 	"log"
-	"log/slog"
-	"os"
 )
 
 type Storage struct {
@@ -28,23 +25,8 @@ func New(con *config.Config) (*Storage, error) {
 	}
 	log.Printf("Succses connect database")
 
-	err = ExecuteSQLFile(connDb)
-	if err != nil {
-		slog.Error("Failed create db", sl.Err(err))
-		panic("good bye")
-	}
-	
 	return &Storage{
 		Db: connDb,
 	}, nil
 }
 
-func ExecuteSQLFile(db *sql.DB) error {
-	content, err := os.ReadFile("storage/init.sql")
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(string(content))
-	return err
-}
--- internal/storage/postgres/user.go
+++ internal/storage/postgres/user.go
@@ -1,206 +1,206 @@
 package postgres
 
-import (
-	"context"
-	"database/sql"
-	"errors"
-	"fmt"
-	"github.com/2pizzzza/authGrpc/internal/domain/models"
-	"github.com/2pizzzza/authGrpc/internal/storage"
-	"log"
-	"time"
-)
+// import (
+// 	"context"
+// 	"database/sql"
+// 	"errors"
+// 	"fmt"
+// 	"github.com/2pizzzza/authGrpc/internal/domain/models"
+// 	"github.com/2pizzzza/authGrpc/internal/storage"
+// 	"log"
+// 	"time"
+// )
 
-func (s *Storage) CreateUser(
-	ctx context.Context, username, email, password string) (models.User, error) {
-	const op = "postgres.user.CreateUser"
+// func (s *Storage) CreateUser(
+// 	ctx context.Context, username, email, password string) (models.User, error) {
+// 	const op = "postgres.user.CreateUser"
 
-	var (
-		id           int64
-		emailTemp    string
-		usernameTemp string
-		isActive     bool
-		isAdminTemp  bool
-	)
+// 	var (
+// 		id           int64
+// 		emailTemp    string
+// 		usernameTemp string
+// 		isActive     bool
+// 		isAdminTemp  bool
+// 	)
 
-	err := s.Db.QueryRow(""+
-		"INSERT INTO public.user (username, email, password) VALUES($1, $2, $3) "+
-		"RETURNING id, username, email, is_active, is_superuser\n",
-		username, email, password).Scan(&id, &usernameTemp, &emailTemp, &isActive, &isAdminTemp)
+// 	err := s.Db.QueryRow(""+
+// 		"INSERT INTO public.user (username, email, password) VALUES($1, $2, $3) "+
+// 		"RETURNING id, username, email, is_active, is_superuser\n",
+// 		username, email, password).Scan(&id, &usernameTemp, &emailTemp, &isActive, &isAdminTemp)
 
-	if err != nil {
-		log.Printf("failed create user: %v op: %s", err, op)
+// 	if err != nil {
+// 		log.Printf("failed create user: %v op: %s", err, op)
 
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	return models.User{
-		Id:       id,
-		Email:    emailTemp,
-		Username: usernameTemp,
-		IsActive: isActive,
-		IsAdmin:  isAdminTemp,
-	}, nil
-}
+// 	return models.User{
+// 		Id:       id,
+// 		Email:    emailTemp,
+// 		Username: usernameTemp,
+// 		IsActive: isActive,
+// 		IsAdmin:  isAdminTemp,
+// 	}, nil
+// }
 
-func (s *Storage) GetUserByEmail(ctx context.Context, email string) (models.User, error) {
-	const op = "postgres.user.GetById"
+// func (s *Storage) GetUserByEmail(ctx context.Context, email string) (models.User, error) {
+// 	const op = "postgres.user.GetById"
 
-	var (
-		idTemp    int64
-		emailTemp string
-		username  string
-		isActive  bool
-		isAdmin   bool
-		createdAt time.Time
-	)
+// 	var (
+// 		idTemp    int64
+// 		emailTemp string
+// 		username  string
+// 		isActive  bool
+// 		isAdmin   bool
+// 		createdAt time.Time
+// 	)
 
-	stmt, err := s.Db.Prepare("SELECT * FROM public.user WHERE email = $1")
+// 	stmt, err := s.Db.Prepare("SELECT * FROM public.user WHERE email = $1")
 
-	defer func(stmt *sql.Stmt) {
-		err := stmt.Close()
-		if err != nil {
+// 	defer func(stmt *sql.Stmt) {
+// 		err := stmt.Close()
+// 		if err != nil {
 
-		}
-	}(stmt)
+// 		}
+// 	}(stmt)
 
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	err = stmt.QueryRow(email).Scan(&idTemp, &username, &emailTemp, &createdAt, &isActive, &isAdmin)
+// 	err = stmt.QueryRow(email).Scan(&idTemp, &username, &emailTemp, &createdAt, &isActive, &isAdmin)
 
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return models.User{}, storage.ErrUserNotFound
-		}
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		if errors.Is(err, sql.ErrNoRows) {
+// 			return models.User{}, storage.ErrUserNotFound
+// 		}
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	return models.User{
-		Id:        idTemp,
-		Username:  username,
-		Email:     emailTemp,
-		CreatedAt: createdAt,
-		IsAdmin:   isAdmin,
-		IsActive:  isActive,
-	}, nil
-}
+// 	return models.User{
+// 		Id:        idTemp,
+// 		Username:  username,
+// 		Email:     emailTemp,
+// 		CreatedAt: createdAt,
+// 		IsAdmin:   isAdmin,
+// 		IsActive:  isActive,
+// 	}, nil
+// }
 
-func (s *Storage) GetUserById(ctx context.Context, id int64) (models.User, error) {
-	const op = "postgres.user.GetById"
+// func (s *Storage) GetUserById(ctx context.Context, id int64) (models.User, error) {
+// 	const op = "postgres.user.GetById"
 
-	var (
-		idTemp    int64
-		email     string
-		username  string
-		isActive  bool
-		isAdmin   bool
-		createdAt time.Time
-	)
+// 	var (
+// 		idTemp    int64
+// 		email     string
+// 		username  string
+// 		isActive  bool
+// 		isAdmin   bool
+// 		createdAt time.Time
+// 	)
 
-	stmt, err := s.Db.Prepare("SELECT * FROM public.user WHERE id = $1")
+// 	stmt, err := s.Db.Prepare("SELECT * FROM public.user WHERE id = $1")
 
-	defer func(stmt *sql.Stmt) {
-		err := stmt.Close()
-		if err != nil {
+// 	defer func(stmt *sql.Stmt) {
+// 		err := stmt.Close()
+// 		if err != nil {
 
-		}
-	}(stmt)
+// 		}
+// 	}(stmt)
 
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	err = stmt.QueryRow(id).Scan(&idTemp, &username, &email, &createdAt, &isActive, &isAdmin)
+// 	err = stmt.QueryRow(id).Scan(&idTemp, &username, &email, &createdAt, &isActive, &isAdmin)
 
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return models.User{}, storage.ErrUserNotFound
-		}
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		if errors.Is(err, sql.ErrNoRows) {
+// 			return models.User{}, storage.ErrUserNotFound
+// 		}
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	return models.User{
-		Id:        idTemp,
-		Username:  username,
-		Email:     email,
-		CreatedAt: createdAt,
-		IsAdmin:   isAdmin,
-		IsActive:  isActive,
-	}, nil
-}
+// 	return models.User{
+// 		Id:        idTemp,
+// 		Username:  username,
+// 		Email:     email,
+// 		CreatedAt: createdAt,
+// 		IsAdmin:   isAdmin,
+// 		IsActive:  isActive,
+// 	}, nil
+// }
 
-func (s *Storage) UpdateUser(
-	ctx context.Context, id int64, newUsername, newEmail string) (models.User, error) {
-	const op = "postgres.user.UpdateUser"
+// func (s *Storage) UpdateUser(
+// 	ctx context.Context, id int64, newUsername, newEmail string) (models.User, error) {
+// 	const op = "postgres.user.UpdateUser"
 
-	stmt, err := s.Db.Prepare("UPDATE public.user SET password = &2 WHERE id = $1")
+// 	stmt, err := s.Db.Prepare("UPDATE public.user SET password = &2 WHERE id = $1")
 
-	defer func(stmt *sql.Stmt) {
-		err := stmt.Close()
-		if err != nil {
+// 	defer func(stmt *sql.Stmt) {
+// 		err := stmt.Close()
+// 		if err != nil {
 
-		}
-	}(stmt)
+// 		}
+// 	}(stmt)
 
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	res, err := stmt.Exec(id, newUsername, newEmail)
+// 	res, err := stmt.Exec(id, newUsername, newEmail)
 
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	rowAffected, err := res.RowsAffected()
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	rowAffected, err := res.RowsAffected()
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	if rowAffected == 0 {
-		return models.User{}, storage.ErrUserNotFound
-	}
+// 	if rowAffected == 0 {
+// 		return models.User{}, storage.ErrUserNotFound
+// 	}
 
-	user, err := s.GetUserById(ctx, id)
+// 	user, err := s.GetUserById(ctx, id)
 
-	if err != nil {
-		return models.User{}, fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return models.User{}, fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	return user, nil
-}
+// 	return user, nil
+// }
 
-func (s *Storage) ChangePassword(ctx context.Context, id, newPassword string) (string, error) {
-	const op = "postgres.user.ChangePassword"
-	stmt, err := s.Db.Prepare("UPDATE public.user SET password = &2 WHERE id = $1")
+// func (s *Storage) ChangePassword(ctx context.Context, id, newPassword string) (string, error) {
+// 	const op = "postgres.user.ChangePassword"
+// 	stmt, err := s.Db.Prepare("UPDATE public.user SET password = &2 WHERE id = $1")
 
-	defer func(stmt *sql.Stmt) {
-		err := stmt.Close()
-		if err != nil {
+// 	defer func(stmt *sql.Stmt) {
+// 		err := stmt.Close()
+// 		if err != nil {
 
-		}
-	}(stmt)
+// 		}
+// 	}(stmt)
 
-	if err != nil {
-		return "", fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return "", fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	res, err := stmt.Exec(id, newPassword)
+// 	res, err := stmt.Exec(id, newPassword)
 
-	if err != nil {
-		return "", fmt.Errorf("%s, %w", op, err)
-	}
+// 	if err != nil {
+// 		return "", fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	rowAffected, err := res.RowsAffected()
-	if err != nil {
-		return "", fmt.Errorf("%s, %w", op, err)
-	}
+// 	rowAffected, err := res.RowsAffected()
+// 	if err != nil {
+// 		return "", fmt.Errorf("%s, %w", op, err)
+// 	}
 
-	if rowAffected == 0 {
-		return "", storage.ErrUserNotFound
-	}
+// 	if rowAffected == 0 {
+// 		return "", storage.ErrUserNotFound
+// 	}
 
-	return "Success changed password", nil
-}
+// 	return "Success changed password", nil
+// }
